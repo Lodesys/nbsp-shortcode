9dacb97d8146dae9c023d52049325302009515eb
Chris Daley <chris@lodesys.com>
2016-05-13 01:21:47 -0700

    Added DocBlock segments.

--- nbsp-shortcode.php
+++ nbsp-shortcode.php
@@ -1,40 +1,63 @@
 <?php
 /**
-Plugin Name: NBSP Shotcode
-Author: Chris Daley
-Author URI: https://www.lodesys.com
-Plugin URI: https://www.lodesys.com/plugin/nbsp-shortcode/
-Requires WP:       3.8
-Version: 0.1.0
-Requires PHP:      5.3
-License: GPLv2 or later
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
-GitHub Plugin URI: https://github.com/Lodesys/nbsp-shortcode.git
-GitHub Branch:     master
-Description: Replaces the `[nbsp]` shortcode with the `&nbsp ;` character entity string.
+ * NBSP Shotcode Plugin
+ *
+ * @package   nbsp-shortcode
+ * @author    Chris Daley <chris@lodesys.com>
+ * @copyright (c) 2016 Chris Daley
+ * @license   GPLv2
+ *
+ * Plugin Name: NBSP Shotcode
+ * Author: Chris Daley
+ * Author URI: https://www.lodesys.com
+ * Plugin URI: https://www.lodesys.com/plugin/nbsp-shortcode/
+ * Requires WP:       3.8
+ * Version: 0.1.0
+ * Requires PHP:      5.3
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * GitHub Plugin URI: https://github.com/Lodesys/nbsp-shortcode.git
+ * GitHub Branch:     master
+ * Description: Replaces the `[nbsp]` shortcode with the `&nbsp ;` character entity string.
  */
 
-// security measure
+/**
+ * security measure
+ */
 defined( 'ABSPATH' ) || die( 'No script kiddies please!' );
 
-// define the class containing the plugin scripts
+/**
+ * Class defining all code for this plugin.
+ */
 class nbsp_shortcode {
 
-	// activate the plugin
+	/**
+	 * Handle plugin activation needs
+	 *
+	 * @return	void
+	 */
 	static function activate() {
+
+		// check for a minimum required WordPress version number
 		if ( version_compare( get_bloginfo( 'version' ), '3.8', '<' ) ) {
 			wp_die( "You must update WordPress to use this plugin!" );
 		}
 	}
 
-	// replace the shortcode
+	/**
+	 * Provides repacement &nbsp; text for the [nbsp] shortcode
+	 *
+	 * @return	string	the replacement &nbsp; text string
+	 */
 	static function insert_nbsp() {
 		return '&nbsp;';
 	}
 
 }
 
-// add the plugin hooks
+/**
+ * Install the hooks for this plugin
+ */
 register_activation_hook( __FILE__, array( 'nbsp_shortcode', 'activate' ) );
 add_shortcode( 'nbsp', array( 'nbsp_shortcode', 'insert_nbsp' ) );
 
--- uninstall.php
+++ uninstall.php
@@ -1,14 +1,29 @@
 <?php
 
-// confirm uninstalling
+/**
+ * Handles item & value cleanup when this plugin is deleted.
+ *
+ * @package   nbsp-shortcode
+ * @author    Chris Daley <chris@lodesys.com>
+ * @copyright (c) 2016 Chris Daley
+ * @license   GPLv2
+ */
+
+/**
+ * confirm deleting / uninstalling plugin
+ */
 if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-// ***  DELETE THIS  ***
+/**
+ * 		***************** YOU MUST DELETE THIS! *****************
+ */
 wp_die(__("UNINSTALL NOT PROGRAMMED FOR THIS PLUGIN! DELETE THIS FILE OR ADD SOME CODE."));
 
-// unwind things
+/**
+ * clean out items & values no longer needed
+ */
 if ( false != get_option( 'something' ) ) {
 	delete_option( 'something' );
 }
